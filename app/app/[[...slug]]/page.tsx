bf19496a9b7cea031413cbcf10c7243d4e934ebe
Bivanjit paul <paulbivanjit@gmail.com>
2026-09-07 17:27:06 +0530

    Fix RONIN app route imports

--- app/app/[[...slug]]/page.tsx
+++ app/app/[[...slug]]/page.tsx
@@ -1,3 +1,3 @@
-import AppShell from '../../../app-shell';
-import AppPage from '../../../app-page';
+import AppShell from '../../app-shell';
+import AppPage from '../../app-page';
 export default async function Page({params}:{params:Promise<{slug?:string[]}>}){const p=await params;return <AppShell><div className="app-content"><AppPage slug={p.slug||[]}/></div></AppShell>}
